5313cbbcaafbc4e7d7f9ba730b9797d227589d90
Anton Smerdov <anton.smerdov95@yandex.ru>
2022-05-01 15:15:12 +0300

    addad new test

--- src/test/java/test/SkippedTests.java
+++ src/test/java/test/SkippedTests.java
@@ -1,10 +1,12 @@
 package test;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("properties")
 public class SkippedTests {
     @Disabled
     @Test
@@ -16,6 +18,13 @@ public class SkippedTests {
     @Test
     void test02() {
         assertTrue(true);
+    }
+
+
+    @Test
+    void someTest() {
+        String browser = System.getProperty("browser" , "opera");
+        System.out.println(browser);
     }
 
 
