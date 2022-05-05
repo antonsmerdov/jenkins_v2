3e8e34b44fb44174f9a63d017603a1c03af4ee4c
Anton Smerdov <anton.smerdov95@yandex.ru>
2022-05-05 21:21:41 +0300

    addad fix string

--- src/test/java/test/SkippedTests.java
+++ src/test/java/test/SkippedTests.java
@@ -8,13 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("properties")
 public class SkippedTests {
-    @Disabled
+
     @Test
     void test01() {
         assertTrue(true);
     }
 
-    @Disabled("With some reason")
     @Test
     void test02() {
         assertTrue(true);
